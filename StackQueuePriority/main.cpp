c04aff9e405577243854e33924c93567016fd260
zareekgalvan <zareek99@gmail.com>
2015-03-03 12:46:55 -0600

    Initial Commit

---
+++ StackQueuePriority/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StackQueuePriority
+//
+//  Created by Zareek Galvan on 3/3/15.
+//  Copyright (c) 2015 Zareek Galvan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
